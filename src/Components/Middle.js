6f75a88d0266d25293dd7867bad4fbc0be59b507
Antsamotady <hasintsilavina@gmail.com>
2023-03-24 16:07:12 +0300

    Right floating links with effects

--- src/Components/Middle.js
+++ src/Components/Middle.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react';
+
+import { TfiLinkedin } from 'react-icons/tfi';
+import { ImStackoverflow } from 'react-icons/im';
+import { BsGithub } from 'react-icons/bs';
+import { SiGmail } from 'react-icons/si';
 
 import '../Stylesheets/middle.css';
 
 function Middle() {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsVisible(false);
+    }, 4000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isVisible]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
   return (
     <div className="middle">
-      Middle Scene
+      <div ref={sectionRef} className={isVisible ? 'visible' : 'hidden'}>
+        <div className="right-floating-links_container">
+          <ul>
+            <li className="right-floating-links-box"><a href="https://linkedin.com/in/antsamotady-tsilavina-618201153"><TfiLinkedin /></a></li>
+            <li className="right-floating-links-box"><a href="https://stackoverflow.com/users/9532910/antsamotady"><ImStackoverflow /></a></li>
+            <li className="right-floating-links-box"><a href="https://github.com/Antsamotady"><BsGithub /></a></li>
+            <li className="right-floating-links-box"><a href="mailto:tsilavinarj02@gmail.com?subject=Interested%20visitor&body=Hi%20Garry!"><SiGmail /></a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
